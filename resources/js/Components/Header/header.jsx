0b118823854deee06aa64eb9caa1ddd09a6b9fa8
Evgeni2306 <Evgeni2306@outlook.com>
2023-01-27 09:47:42 +0500

    Изменение хедера для презентации

--- resources/js/Components/Header/header.jsx
+++ resources/js/Components/Header/header.jsx
@@ -16,27 +16,31 @@ export default function Header({}) {
                 </div>
                 <nav className="header__menu menu">
                     <ul className="menu__list">
+
                         <li className="menu__item">
-                            Тренировочное собеседование
+                                <a href={route('interviewSphere')}>
+                                    <div className="linkItem" >
+                                        Тренировочное собеседование
+                                    </div>
+                                </a>
                         </li>
-                        <li className="menu__item">База знаний</li>
-                        <li className="menu__item">Избранное</li>
                         <li className="menu__item">
-                            <img
-                                className="menu__item-icon"
-                                src="/img/plus.svg"
-                            />
-                            Пополнить базу знаний
+                            <a href={route('getProfessionsForKnowledgeBase')}>
+                                <div className="linkItem" >
+                                    База знаний
+                                </div>
+                            </a>
                         </li>
+
                     </ul>
                 </nav>
                 <nav className="header__user">
-                    <img
-                        className="header__user-img"
-                        src="/img/user.png"
-                        alt="user"
-                    />
-                    <span className="header__user-arrow"></span>
+                    {/*<img*/}
+                    {/*    className="header__user-img"*/}
+                    {/*    src="/img/user.png"*/}
+                    {/*    alt="user"*/}
+                    {/*/>*/}
+                    {/*<span className="header__user-arrow"></span>*/}
                 </nav>
             </div>
         </div>
